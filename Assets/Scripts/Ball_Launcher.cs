a6065a928be029f4b877019473eb356fed099f6c
bknie1 <brandonknieriem26@gmail.com>
2017-07-20 13:21:44 -0400

    Spawn offset, ball destroyer.

--- Assets/Scripts/Ball_Launcher.cs
+++ Assets/Scripts/Ball_Launcher.cs
@@ -6,6 +6,8 @@ public class Ball_Launcher : MonoBehaviour {
 
 	public GameObject basketball;
 	public float velocity = 10.5f;
+	public float timer = 10.0f;
+	Vector3 offset = new Vector3(0, .20f, 0);
 	//------------------------------------------------------------------------------------------
 	// Use this for initialization
 	void Start () {
@@ -16,17 +18,18 @@ public class Ball_Launcher : MonoBehaviour {
 	// Spawns a ball and assigns direction based on user input.
 	void Update () {
 		//if (Input.GetMouseButtonDown (0)) Spawn(); // On left click, spawn ball.
-		if (Input.GetButtonDown("Fire1")) Spawn();
+		if (Input.GetButtonDown("Fire1")) Spawn_Ball();
 	}
 	//------------------------------------------------------------------------------------------
-	// Creates a basketball.
-	void Spawn() {
+	// Creates a basketball, destroys after delay.
+	void Spawn_Ball() {
 		Camera camera = GetComponentInChildren<Camera>();
 		GameObject ball = Instantiate (basketball);
-		ball.transform.position = this.transform.position; // Sets ball position on camera.
+		ball.transform.position = this.transform.position - offset; // Sets ball position on camera.
 		Rigidbody rb = ball.GetComponent<Rigidbody> ();
 		// Launches ball in camera direction.
 		rb.velocity = camera.transform.rotation * Vector3.forward * velocity;
+		Destroy(ball, timer);
 	}
 	//------------------------------------------------------------------------------------------
 }
