6e1092a240b595f254bbc054c606fe9375f7d205
bknie1 <brandonknieriem26@gmail.com>
2018-01-31 19:25:10 +0000

    Fixed scene load lighting.

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		// Load the next scene.
+		SceneManager.LoadScene("02_Game");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
