a094dfde101b2d4302c286b7dfa2efc41185d4b2
bknie1 <brandonknieriem26@gmail.com>
2018-02-02 04:12:07 +0000

    Refactored menu to be VR appropriate.

---
+++ Assets/Scripts/NewGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NewGame : MonoBehaviour {
+	//-------------------------------------------------------------------------
+	void Start () {
+		
+	}
+	//-------------------------------------------------------------------------
+	void TaskOnClick () {
+
+	}
+	//-------------------------------------------------------------------------
+}
